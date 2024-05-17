f89ca70cb15d1dfe3e176850d3c9eba66fa2e4e6
ecehn <eric.chen5@yahoo.com>
2024-05-17 13:54:41 -0600

    Added logging

--- base.py
+++ base.py
@@ -1,22 +1,34 @@
 import csv
 import sqlite3
 import json
+import logging
+import logging.config
+
+# Set logging configuration
+logging.config.fileConfig('temp.conf')
+ 
+# Create logger
+logger = logging.getLogger()
 
 # Connect to the DB
 conn = sqlite3.connect('pokemon.db')
 cursor = conn.cursor()
 
-# Create table for Pokemon data
-cursor.execute("DROP TABLE IF EXISTS Pokemon")
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Pokemon (
-        Name TEXT,
-        Type_1 TEXT,
-        Type_2 TEXT,
-        Generation INTEGER,
-        Legendary TEXT
-    )
-''')
+try:
+    # Create table for Pokemon data
+    cursor.execute("DROP TABLE IF EXISTS Pokemon")
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Pokemon (
+            Name TEXT,
+            Type_1 TEXT,
+            Type_2 TEXT,
+            Generation INTEGER,
+            Legendary TEXT
+        )
+    ''')
+    logger.info('Sucessfully created Pokemon table')
+except:
+    logger.error("Failed to create Pokemon table")
 
 # Read the CSV and insert into the DB
 with open('pokemon.csv', mode='r') as file:
@@ -31,13 +43,16 @@ with open('pokemon.csv', mode='r') as file:
             generation = int(row['Generation']) 
             legendary = row['Legendary']
             
-            # Insert the data into the table
-            cursor.execute('''
-                INSERT INTO Pokemon (Name, Type_1, Type_2, Generation, Legendary) VALUES (?, ?, ?, ?, ?)
-            ''', (name, type1, type2, generation, legendary))
-        print("Pokemon added to the DB successfully")
+            try:
+                # Insert the data into the table
+                cursor.execute('''
+                    INSERT INTO Pokemon (Name, Type_1, Type_2, Generation, Legendary) VALUES (?, ?, ?, ?, ?)
+                ''', (name, type1, type2, generation, legendary))
+                logger.debug("%s added to the DB successfully", name)
+            except:
+                logger.debug("Failed to add %s to the DB", name)
     except:
-        print("Adding to DB failed")
+        logger.error("Reading from CSV failed")
 
 
 # Select all legendary Pokemon
@@ -48,20 +63,29 @@ legendary_pokemon = cursor.fetchall()
 
 conn.close()
 
-# Convert the fetched data to a list of dictionaries
-legendary_pokemon_list = []
-for pokemon in legendary_pokemon:
-    legendary_pokemon_list.append({
-        "Name": pokemon[0],
-        "Type_1": pokemon[1],
-        "Type_2": pokemon[2],
-        "Generation": pokemon[3],
-        "Legendary": pokemon [4]
-    })
+try:
+    # Convert the fetched data to a list of dictionaries
+    legendary_pokemon_list = []
+    for pokemon in legendary_pokemon:
+        legendary_pokemon_list.append({
+            "Name": pokemon[0],
+            "Type_1": pokemon[1],
+            "Type_2": pokemon[2],
+            "Generation": pokemon[3],
+            "Legendary": pokemon [4]
+        })
+    logging.info("Successfully fetched legendaries")
+except:
+    logging.error("Failed to fetch legendaries")
 
-# Write the legendary Pokémon data to a JSON file
-with open('legendary_pokemon.json', 'w') as json_file:
-    json.dump(legendary_pokemon_list, json_file, indent=4)
+try:
+    # Write the legendary Pokémon data to a JSON file
+    with open('legendary_pokemon.json', 'w') as json_file:
+        json.dump(legendary_pokemon_list, json_file, indent=4)
+    logging.info("Pokemon JSON created")
+except:
+    logging.error("Failed to create Pokemon JSON")
+
 
 print("Legendary Pokémon data has been written to legendary_pokemon.json")
 
